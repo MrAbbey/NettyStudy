5bd62267dffec31038ffb4c5e0c71a330b121573
xuanxinpl <penglei_112@163.com>
2015-08-26 21:50:45 +0800

    Netty学习及笔记01

---
+++ DiscardServer/src/main/java/com/netty/study/discardserver/handler/DiscardServerHandler.java
@@ -0,0 +1,39 @@
+package com.netty.study.discardserver.handler;
+
+/**
+ * Created by pl on 2015/8/26.
+ */
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ *用于处理服务端I/O事件的管道
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) { // (1)
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }
+        // 默认丢弃接收的数据
+        //((ByteBuf) msg).release(); // (3)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // 当异常发生的时候关闭连接
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
+}
